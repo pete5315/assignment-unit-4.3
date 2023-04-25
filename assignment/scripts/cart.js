11c80782734a3691a2bd00054fe4149467950dc3
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-04-25 15:02:28 -0500

    Added comments

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -4,21 +4,22 @@ console.log('***** Cart Functions *****');
 let basket=[];
 console.log(`Basket is ${basket}`);
 
+//Function to add an inputted item to the basket
 function addItem (item) {
-    if (isFull()) {
+    if (isFull()) { //check if the basket is full
         console.log(`The basket is full.`)
         return false;
-    } else {
+    } else { //if the basket is not full, add the item  to the basket
         basket.push(item);
         return true;
     }
 }
-
+//test the add item function
 addItem(2);
 addItem(5);
-
 console.log(`Basket is ${basket}`);
 
+//function to list the items in the basket
 function listItems() {
     console.log(`List items are:`)
     let i=0
@@ -28,16 +29,21 @@ function listItems() {
     }
 }
 
+//test the list items function
 listItems();
 
+//function to empty the basket
 function empty() {
     basket=[];
 }
+//test the empty function
 empty();
 console.log(`Basket is ${basket}`);
-addItem(5);
+
+//establish our constant maximum
 const maxItems=5;
 
+//function to check if the basket is full
 function isFull() {
     if (basket.length>=5) {
         return true;
@@ -45,9 +51,6 @@ function isFull() {
     else {return false}
 }
 
-addItem(1);
-addItem(2);
-addItem(3);
 
 function removeItem(item) {
     if (basket.indexOf(item)===-1) {
@@ -57,12 +60,18 @@ function removeItem(item) {
         return basket.splice(basket.indexOf(item),1);
     }
 }
+
+//refilling the basket to check if the remove item function is working
+addItem(5);
+addItem(1);
+addItem(2);
+addItem(3);
 removeItem(2)
 console.log(`Basket is ${basket}`);
 console.log("Item removed was " + removeItem(5));
 console.log(`Basket is ${basket}`);
 
-
+//adding additional items to test the is full function
 addItem(1);
 addItem(2);
 addItem(3);
